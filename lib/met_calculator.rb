49657e95ea31335097b47a11e91f4cdf6ef5aadc
Cosmin Enache <44037571+cosmin-enache@users.noreply.github.com>
2021-11-25 15:36:39 +0200

    Added 'calories' calculation and modified tests
    The tests were modified to check whether the calculations for each activity type are valid.

--- lib/met_calculator.rb
+++ lib/met_calculator.rb
@@ -5,4 +5,46 @@ require_relative "met_calculator/version"
 module MetCalculator
   class Error < StandardError; end
   # Your code goes here...
+  
+  # converts activity symbol into its respective 'MET' value
+  def self.met_for_activity(activity)
+    activities = {
+      running: 8,
+      cycling: 7.5,
+      swimming: 5.8
+    }
+
+    if not activities.include? activity then
+      raise "Activity `#{activity}` not found!"
+      return
+    end
+
+    return activities[activity].to_f
+  end
+
+  # checks if `value` is either an int or a float
+  def self.is_number?(value)
+    return value.is_a?(Float) || value.is_a?(Integer)
+  end
+  
+  def self.calories(activity, body_mass_kg, time_performing_hours)
+    begin
+      # Error check
+      if not self.is_number? body_mass_kg 
+        raise "`#{body_mass_kg}` is not a number!" 
+      end
+
+      if not self.is_number? time_performing_hours
+        raise "`#{time_performing_hours}` is not a number!"
+      end
+      
+      # Calculate
+      met_value = self.met_for_activity(activity)
+      calories = met_value * body_mass_kg * time_performing_hours
+
+      return calories
+    rescue Exception => errorMessage
+      puts "Error! -> #{errorMessage}"
+    end
+  end
 end
--- spec/met_calculator_spec.rb
+++ spec/met_calculator_spec.rb
@@ -4,18 +4,24 @@ RSpec.describe MetCalculator do
   it "has a version number" do
     expect(MetCalculator::VERSION).not_to be nil
   end
-
+  
   describe "calculating calories burned using Metabolic Equivalent of Task values" do
     it "calculates a value for running" do
-      expect(false).to eq(true)
+      expect(
+        MetCalculator.calories :running, 80, 1
+      ).to eq(640.0)
     end
 
     it "calculates a value for cycling" do
-      expect(false).to eq(true)
+      expect(
+        MetCalculator.calories :cycling, 90, 2
+      ).to eq(1350.0)
     end
 
     it "calculates a value for swimming" do
-      expect(false).to eq(true)
+      expect(
+        MetCalculator.calories :swimming, 110, 3
+      ).to eq(1914.0)
     end
   end
 end
